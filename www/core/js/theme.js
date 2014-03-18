fbddf71afa8616b270d629fb1d21acaadb959c56
Graham Blevins <hgblevins@gmail.com>
2014-03-18 00:22:10 -0400

    support for base url

--- www/core/js/theme.js
+++ www/core/js/theme.js
@@ -1,9 +1,6 @@
 require.config({
 
-	baseUrl: function (window) {
-		window.thetheme = window.thetheme || {};
-		return [window.thetheme.urlBase, '/core'].join('');
-	}(this),
+	baseUrl: '/core',
 
 	deps: [
 		'lib/html5shiv/dist/html5shiv'
