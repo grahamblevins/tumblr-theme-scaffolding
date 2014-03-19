e6b439039a4e488b97f2fe416239f4464baa7aa0
Graham Blevins <hgblevins@gmail.com>
2014-03-19 10:54:55 -0400

    renamed thetheme global to scaffold, moved base url to pre-require config

--- www/core/js/theme.js
+++ www/core/js/theme.js
@@ -1,10 +1,5 @@
 require.config({
 
-	baseUrl: function (window) {
-		window.thetheme = window.thetheme || {};
-		return [window.thetheme.urlBase, '/core'].join('');
-	}(this),
-
 	deps: [
 		'lib/html5shiv/dist/html5shiv'
 	],
