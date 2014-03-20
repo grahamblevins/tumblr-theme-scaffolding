c1893718a2c933d420d001c9d5964c9ab44f6321
Graham Blevins <hgblevins@gmail.com>
2014-03-20 15:54:50 -0400

    removed http_path option

--- config.rb
+++ config.rb
@@ -1,26 +1,7 @@
-# Require any additional compass plugins here.
-
-# Set this to the root of your project when deployed:
-http_path = "/"
+cache_dir = ".sass-cache"
 css_dir = "www/core/css"
 extensions_dir = "www/core/lib"
 images_dir = "www/core/img"
 javascripts_dir = "www/core/js"
 sass_dir = "sass"
-cache_dir = ".sass-cache"
-
-
-# You can select your preferred output style here (can be overridden via the command line):
 output_style = :expanded
-
-# To enable relative paths to assets via compass helper functions. Uncomment:
-# relative_assets = true
-
-# To disable debugging comments that display the original location of your selectors. Uncomment:
-# line_comments = false
-
-# If you prefer the indented syntax, you might want to regenerate this
-# project again passing --syntax sass, or you can uncomment this:
-# preferred_syntax = :sass
-# and then run:
-# sass-convert -R --from scss --to sass sass scss && rm -rf sass && mv scss sass
