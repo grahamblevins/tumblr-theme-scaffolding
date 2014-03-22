899cee6fbd5ea4ca2a6ae84d76fe6b049bf08ad5
Graham Blevins <hgblevins@gmail.com>
2014-03-21 21:33:13 -0400

    adding jquery dep, testing private jquery module

--- www/core/js/theme.js
+++ www/core/js/theme.js
@@ -1,11 +1,22 @@
 require.config({
 
 	deps: [
+		'jquery',
 		'lib/html5shiv/dist/html5shiv'
 	],
 
+	map: {
+		'*': {
+			'jquery': 'app/lib/jquery/jquery-private'
+		},
+		'app/lib/jquery/jquery-private': {
+			'jquery': 'jquery'
+		}
+    },
+
 	paths: {
-		'app': 'js'
+		'app': 'js',
+		'jquery': 'lib/jquery/dist/jquery'
 	},
 
 	shim: {}
