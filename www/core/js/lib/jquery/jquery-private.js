5345f9010f5253eea418c2162a80a84d1cf21a99
Graham Blevins <hgblevins@gmail.com>
2014-03-21 21:55:32 -0400

    remove private jquery

--- www/core/js/lib/jquery/jquery-private.js
+++
@@ -1,4 +0,0 @@
-define(['jquery'], function (jQuery) {
-
-	return jQuery.noConflict();
-});
--- www/core/js/theme.js
+++ www/core/js/theme.js
@@ -1,22 +1,11 @@
 require.config({
 
 	deps: [
-		'jquery',
 		'lib/html5shiv/dist/html5shiv'
 	],
 
-	map: {
-		'*': {
-			'jquery': 'app/lib/jquery/jquery-private'
-		},
-		'app/lib/jquery/jquery-private': {
-			'jquery': 'jquery'
-		}
-    },
-
 	paths: {
-		'app': 'js',
-		'jquery': 'lib/jquery/dist/jquery'
+		'app': 'js'
 	},
 
 	shim: {}
